75e68e1f17aa971d571881e7391fafeb4b8cbd04
Daniel Rojas <danielrojasvalda@gmail.com>
2023-11-15 00:05:22 -0500

    add 29-5 testing jest and node practice

--- 29-5_testing_jest_node/node-markov/markov.js
+++ 29-5_testing_jest_node/node-markov/markov.js
@@ -10,6 +10,7 @@ class MarkovMachine {
     this.words = words.filter(c => c !== "");
     this.makeChains();
   }
+  
 
   /** set markov chains:
    *
@@ -17,13 +18,46 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    this.chains = {};
+    for (let i = 0; i < this.words.length - 1; i++) {
+      const word = this.words[i];
+      const nextWord = this.words[i + 1];
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+      this.chains[word].push(nextWord);
+    }  
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
-  }
+    const startIdx = Math.floor(Math.random() * (this.words.length - 1));
+    let currentWord = this.words[startIdx];
+    let generatedText = [currentWord];
+
+    for (let i = 1; i < numWords; i++) {
+      const possibleNextWords = this.chains[currentWord];
+      if (!possibleNextWords || possibleNextWords.length === 0) {
+        break;
+      }
+      const nextWordIdx = Math.floor(Math.random() * possibleNextWords.length);
+      const nextWord = possibleNextWords[nextWordIdx];
+      if (nextWord === null) {
+        break;
+      }
+      generatedText.push(nextWord);
+      currentWord = nextWord;
+    }
+
+    return generatedText.join(' ');
+  }  
 }
+
+// Example usage:
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm.makeText());
+
+module.exports = MarkovMachine;
+
