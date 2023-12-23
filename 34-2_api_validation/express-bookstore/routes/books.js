5bd8898a46083fd4439dff9aad7fcb5fee239dcd
Daniel Rojas <danielrojasvalda@gmail.com>
2023-12-23 16:18:05 -0500

    add practice Unit 34.2 API validation practice

---
+++ 34-2_api_validation/express-bookstore/routes/books.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const Book = require("../models/book");
+
+const router = new express.Router();
+
+
+/** GET / => {books: [book, ...]}  */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  => {book: book} */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /   bookData => {book: newBook}  */
+
+router.post("/", async function (req, res, next) {
+  try {
+    const book = await Book.create(req.body);
+    return res.status(201).json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.update(req.params.isbn, req.body);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
