27e24337dcd46414bb78072b0a0c897af549112b
Daniel Rojas <danielrojasvalda@gmail.com>
2024-03-18 22:29:14 -0400

    add sorting algorithms

---
+++ 42-4_sorting_algorithms/dsa-sorting/bubble.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
